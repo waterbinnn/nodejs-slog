1b440dab708d4480fe58cd1c41007777c91a4422
Subin Lee <ewaterbinn@gmail.com>
2024-10-05 02:15:06 +0900

    feat: env, error/success message 상수 작성

---
+++ src/lib/constants.ts
@@ -0,0 +1,103 @@
+import { Secret } from 'jsonwebtoken';
+
+const FIRST_PWD = process.env.FIRST_PWD;
+const ROUND = Number(process.env.HASH_ROUND);
+const SECRET_KEY = process.env.SECRET_KEY as Secret;
+const ACCENSS_TOKEN_EXPIRES = process.env.ACCENSS_TOKEN_EXPIRES;
+const REFRESH_TOKEN_EXPIRES = process.env.REFRESH_TOKEN_EXPIRES;
+
+const ERROR_MESSAGE = {
+  badRequest: {
+    success: false,
+    status: 400,
+    message: 'Bad Request',
+  },
+  likeAddError: {
+    success: false,
+    status: 400,
+    message: 'Already Add Like',
+  },
+  likeCancelError: {
+    success: false,
+    status: 400,
+    message: 'No Like',
+  },
+  unauthorized: {
+    success: false,
+    status: 401,
+    message: 'Unauthorized',
+  },
+  invalidToken: {
+    success: false,
+    status: 401,
+    message: 'Invalid token',
+  },
+  notExpired: {
+    success: false,
+    status: 401,
+    message: 'Token Not Expired',
+  },
+  forbidden: {
+    success: false,
+    status: 403,
+    message: 'Forbidden',
+  },
+  alreadySignup: {
+    success: false,
+    status: 403,
+    message: 'Already Sign Up',
+  },
+  notFound: {
+    success: false,
+    status: 404,
+    message: 'Not Found',
+  },
+  preconditionFailed: {
+    success: false,
+    status: 412,
+    message: 'Precondition Failed',
+  },
+  serverError: {
+    success: false,
+    status: 500,
+    message: 'Internal Server Error',
+  },
+} as const;
+
+const SUCCESS_MESSAGE = {
+  loginOk: {
+    success: true,
+    status: 201,
+    message: 'Login Ok!',
+  },
+  logoutOk: {
+    success: true,
+    status: 205,
+    message: 'Logout success!',
+  },
+  refreshToken: {
+    success: true,
+    status: 201,
+    message: 'refresh success',
+  },
+  accessTokenOk: {
+    success: true,
+    status: 200,
+    message: 'access token ok',
+  },
+  registerOk: {
+    success: true,
+    status: 201,
+    message: 'register success!',
+  },
+} as const;
+
+export {
+  ERROR_MESSAGE,
+  SUCCESS_MESSAGE,
+  FIRST_PWD,
+  ROUND,
+  SECRET_KEY,
+  ACCENSS_TOKEN_EXPIRES,
+  REFRESH_TOKEN_EXPIRES,
+};
